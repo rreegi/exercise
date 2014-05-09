47b42b3c390008cfc42432536474f642bd42d176
rreegi <ram89621@gmail.com>
2014-05-09 09:34:46 -0400

    ifstatement

---
+++ ifstatement/ifstatement/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  ifstatement
+//
+//  Created by User1 on 2014-05-09.
+//  Copyright (c) 2014 Juice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int number;
+        NSLog(@"Type in your number: ");
+        scanf("%i",&number);
+      
+        if(number < 0) {
+            number = -number;
+        }
+        NSLog(@"The absolute value is %i", number);
+    }
+    return 0;
+}
+
